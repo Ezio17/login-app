3ae1cd285ef4e668ffc1daab201479375d67ecd7
ezio17 <xd_ezio@ukr.net>
2019-04-16 19:54:36 +0300

    build was added

---
+++ build/precache-manifest.82d0e2fc6729725549915ce6de01a48a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f62e6898ab294b32815",
+    "url": "/static/css/main.3345d8a9.chunk.css"
+  },
+  {
+    "revision": "3f62e6898ab294b32815",
+    "url": "/static/js/main.c36c0de4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c220ec051ed0b1219098",
+    "url": "/static/js/2.a8ba0a71.chunk.js"
+  },
+  {
+    "revision": "28903b4e5ede2bca858db81e4a5ea8fd",
+    "url": "/index.html"
+  }
+];
